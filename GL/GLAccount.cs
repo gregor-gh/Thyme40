2c80f3312c18b5e1bb0d2f89e0918f0847b8818b
Gregor <73136777+gregor-gh@users.noreply.github.com>
2021-10-25 00:32:46 +0100

    Initial commit

---
+++ GL/GLAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thyme40.BLL
+{
+    class GLAccount
+    {
+        public string AccountNumber { get; set; }
+        public string AccountName { get; set; }
+        public decimal AccountBalance { get; set; }
+
+    }
+}
---
+++ GL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
