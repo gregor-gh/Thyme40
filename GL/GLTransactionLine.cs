e6ddf2bdffd6f741a09f7065d56fac0dd70cc9b0
Gregor <73136777+gregor-gh@users.noreply.github.com>
2021-11-03 00:14:19 +0000

    Added GLTransactionLine

---
+++ GL/GLTransactionLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thyme40.BLL
+{
+    class GLTransactionLine
+    {
+        public GLAccount Account { get; }
+        public decimal Amount { get; }
+        public GLTransactionLine(GLAccount account, decimal amount)
+        {
+            Account = account;
+            Amount = amount;
+        }
+    }
+}
